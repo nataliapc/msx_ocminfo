6a7b26dac13e2c36ef659d8fb8630530c9c4aca6
nataliapc <natalia.pujol@gmail.com>
2025-04-05 12:00:58 +0200

    Adding visibility to u2-SX machine

--- includes/ocminfo.h
+++ includes/ocminfo.h
@@ -81,7 +81,7 @@ static const char *ocminfoVersionStr = "\x13 ocminfo v"VERSION" \x14";
 
 #define MACHINETYPE_UNKNOWN	 	15		// Unknown machine
 static const char *machineTypeStr[16] = {
-	"1chipMSX", "Zemmix Neo/SX-1", "SM-X/MC2P", "SX-2", "SM-X Mini/SM-X HB",
+	"1chipMSX", "Zemmix Neo/SX-1", "SM-X/MC2P", "SX-2", "SM-X Mini/SM-X HB/u2-SX",
 	"DE0CV+DEOCM", "SX-E/SX-Lite", "Unknown", "Unknown", "Unknown", "Unknown",
 	"Unknown", "Unknown", "Unknown", "Unknown", "Unknown"
 };
